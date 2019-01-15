2841f5bd9a93c729ee1167a6ec2de4cd7baa1bc6
ma7modGamel <mahmoudgamel10@gmail.com>
2019-01-15 06:14:58 +0200

    CreateXmlLogin

---
+++ app/src/main/java/com/example/simplestore/LogInUsers/LogInActivity.java
@@ -0,0 +1,16 @@
+package com.example.simplestore.LogInUsers;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.simplestore.R;
+
+public class LogInActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+    }
+}

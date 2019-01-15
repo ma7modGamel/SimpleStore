8ebde05a6c6dc02fd5140b4690469f6897d78ea2
ma7modGamel <mahmoudgamel10@gmail.com>
2019-01-15 07:30:42 +0200

    finishSplash

--- app/src/main/java/com/example/simplestore/LogInUsers/LogInActivity.java
+++ app/src/main/java/com/example/simplestore/LogInUsers/LogInActivity.java
@@ -9,7 +9,7 @@ public class LogInActivity extends AppCompatActivity {
     @Override
     protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_splash);
 
 
     }
